874df9077e82c20e194b19e1326e584961297a22
Igor Lopes <claudioigorai@gmail.com>
2023-05-18 22:58:03 -0300

    Implementação de migration e anotações JPA. Mapeamento do banco.

--- src/main/java/med/voll/api/medico/DadosCadastroMedicos.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedicos.java
@@ -1,8 +1,31 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
+public record DadosCadastroMedicos(
+    
+    @NotBlank
+    String nome,
+    
+    @NotBlank
+    @Email
+    String email, 
 
-public record DadosCadastroMedicos(String nome, 
-String email, String crm, Especialidade especialidade, DadosEndereco endereco){
+    @NotBlank
+    String telefone,
+    @NotBlank
+    @Pattern(regexp = "\\d{4,6}")
+    String crm, 
+
+    @NotNull
+    Especialidade especialidade, 
+    
+    @NotNull
+    @Valid
+    DadosEndereco endereco){
     
 }
---
+++ src/main/resources/db/migration/V2__alter-table-telefone-medicos.sql
@@ -0,0 +1,1 @@
+alter table medicos add telefones varchar(20) not null;
